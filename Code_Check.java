ffd38f5955f3fed48d9de2875a5ee12b5e87866a
OrcaShamuDeluxeU <73401174+OrcaShamuDeluxeU@users.noreply.github.com>
2022-01-29 21:09:53 -0800

    Add files via upload

---
+++ Code_Check.java
@@ -0,0 +1,11 @@
+package Main;
+import java.util.*;
+import java.io.*;
+public class Code_Check {
+	public static void main(String[] args) throws Exception {
+		Scanner stdin = new Scanner(new File("2.in"));
+		PrintWriter out = new PrintWriter(new FileWriter("2.out"));
+		int n = stdin.nextInt();
+		System.out.println(n);
+	}
+}
